c848ba2bdbdea61258ab2fb971364b6eadcba59b
parkjm <ppjjmmhp@gmail.com>
2021-10-31 03:28:07 +0900

    [ADD] board 수정, 삭제 [ADD] 커스텀 validation Pipe 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Board } from './boards.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
 
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation';
 
 @Controller('boards')
 export class BoardsController {
@@ -12,11 +14,28 @@ export class BoardsController {
         return this.boardsService.getAllBoards();
     }
     @Post()
+    @UsePipes(ValidationPipe)
     createBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
+        // @Body('title') title: string,
+        // @Body('description') description: string
+        @Body() createBoradDto: CreateBoardDto
         ): Board 
     { // @Body('title') 이면 title만, 
-        return this.boardsService.createBoard(title,description)
+        return this.boardsService.createBoard(createBoradDto)
+    }
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board{
+        return this.boardsService.getBoardById(id);
+    }
+    @Delete('/:id')
+    deleteBoard(@Param('id') id:string){
+        this.boardsService.deleteBoard(id);
+    }
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status',BoardStatusValidationPipe) status: BoardStatus
+    ){
+        return this.boardsService.updateBoardStatus(id,status);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,22 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Param } from '@nestjs/common';
 import { Board,BoardStatus } from './boards.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+import {v1 as uuid} from 'uuid';
 
 @Injectable()
 export class BoardsService {
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
+    }
     private boards: Board[]= [];
 
     getAllBoards(): Board[]{
         return this.boards;
     }
 
-    createBoard(title: string, description: string): Board{
+    createBoard(createBoradDto: CreateBoardDto): Board{
+        const {title,description} = createBoradDto;
         const board: Board = {
-            id: '333',
-            title, // title:title과 동일
+            id: uuid(),
+            title, // title:createBoradDto.title과 동일
             description, // description:description과 동일
             status: BoardStatus.PUBLIC
         }
         this.boards.push(board);
         return board;
+    }
+    getBoardById(id: string): Board{
+        const found = this.boards.find((board)=>board.id===id);
+        if(!found){
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+        return found;
+    }
+    deleteBoard(id: string): void{
+        const found = this.boards.find((board)=>board.id===id);
+        this.boards.filter((board)=>board.id!==found.id);
     }
 }
